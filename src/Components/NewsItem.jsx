2e16605125f6d1c6f1ad97dab2290f251f962ef2
Agnibho <sahaagnibho@gmail.com>
2024-06-11 21:22:00 +0530

    deploy react to github

---
+++ src/Components/NewsItem.jsx
@@ -0,0 +1,17 @@
+import image from "../assets/news.jpg"
+
+const NewsItem = ({title,description,src,url}) => {
+  return (
+    <div>
+        <div className="card bg-light text-dark mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"340px"}}>
+  <img src={src?src:image} style={{height:"200px" ,width:"325px"}} className="card-img-top" alt={description}/>
+  <div className="card-body">
+    <h5 className="card-title">{title.slice(0,50)}</h5>
+    <p className="card-text">{description?description.slice(0,90):"Click the button below to read more"}</p>
+    <a href={url} className="btn btn-primary">Read More</a>
+  </div>
+</div>
+    </div>
+  )
+}
+export default NewsItem;
